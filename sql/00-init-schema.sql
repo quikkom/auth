379a54fbc98968759048406fb8a08dde0d13c47e
Muhammed Karamuk <mkaramuk@proton.me>
2024-07-27 23:05:59 +0300

    fix: Syntax error in sql script

--- sql/00-init-schema.sql
+++ sql/00-init-schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS  users ON auth (
+CREATE TABLE IF NOT EXISTS  users (
     username        VARCHAR(30) PRIMARY KEY NOT NULL,
     email           VARCHAR(320) NOT NULL,
     "password"      VARCHAR(100) NOT NULL
